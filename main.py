2b3cf63777e2ff732d22b7d94e32318acc89b223
Kasidis Sae-koh <85777908+marshmeowllo@users.noreply.github.com>
2025-04-18 20:09:21 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import time
+import json
+
+import model_lodder
+
+st.header('Dungeons and Dragons', divider="gray")
+
+def chat_stream(user_input):
+    response = model_lodder.generate_response_with_role(user_input=user_input)
+
+    for char in response:
+        yield char
+        time.sleep(0.005)
+
+def save_feedback(index):
+    st.session_state.history[index]["feedback"] = st.session_state[f"feedback_{index}"]
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+for i, message in enumerate(st.session_state.history):
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+        if message["role"] == "assistant":
+            feedback = message.get("feedback", None)
+            st.session_state[f"feedback_{i}"] = feedback
+            st.feedback(
+                "thumbs",
+                key=f"feedback_{i}",
+                disabled=feedback is not None,
+                on_change=save_feedback,
+                args=[i],
+            )
+
+if prompt := st.chat_input("Say something"):
+    with st.chat_message("user"):
+        st.write(prompt)
+    st.session_state.history.append({"role": "user", "content": prompt})
+
+    with st.chat_message("assistant"):
+        response = st.write_stream(chat_stream(st.session_state.history))
+        st.feedback(
+            "thumbs",
+            key=f"feedback_{len(st.session_state.history)}",
+            on_change=save_feedback,
+            args=[len(st.session_state.history)],
+        )
+    st.session_state.history.append({"role": "assistant", "content": response})
+
+print('------------------------------------------------')
+print(st.session_state)
+print('------------------------------------------------')
+
+st.sidebar.header('Settings')
+
+with st.sidebar:
+    st.download_button(
+        label="Download Conversation", 
+        data=json.dumps(st.session_state.history, indent=2),
+        file_name="conversation.json",
+        mime="application/json",
+        icon=":material/download:"
+    )
---
+++ model_lodder.py
@@ -0,0 +1,83 @@
+import torch
+
+from lightning import Fabric
+from huggingface_hub import login
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from peft import get_peft_model, LoraConfig, PeftModelForCausalLM
+
+student_model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
+
+quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_use_double_quant=True,
+    llm_int8_enable_fp32_cpu_offload=True
+)
+
+lora_config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    target_modules=["q_proj", "k_proj"],
+    lora_dropout=0.2,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
+
+student_model = AutoModelForCausalLM.from_pretrained(student_model_name, device_map="auto", quantization_config=quant_config,)
+tokenizer = AutoTokenizer.from_pretrained(student_model_name)
+
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+for param in student_model.parameters():
+  param.requires_grad = False
+
+student_model.gradient_checkpointing_enable()
+student_model.enable_input_require_grads()
+
+
+lora_model = get_peft_model(student_model, lora_config)
+
+fabric = Fabric(accelerator="cuda", devices=1, precision="bf16")
+device = fabric.device
+fabric.launch()
+torch.set_float32_matmul_precision("medium")
+
+model = PeftModelForCausalLM.from_pretrained(lora_model, "./best")
+
+def generate_response_with_role(model=model, user_input='', max_length=4096):
+    messages = [
+      {"role": "system", "content": "in a text-based adventure (Dungeons and Dragons).\nYour job is to narrate the adventure and respond to the player's actions.\nWhen you anwser to player you must answer in proper markdown format. (heading, table, bold, italic, paragraph, blockquotes)"},
+    ]
+
+    for converstion in user_input:
+      messages.append(converstion)
+
+    input_ids = tokenizer.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        return_tensors="pt"
+    ).to(model.device)
+
+    terminators = [
+        tokenizer.eos_token_id,
+        tokenizer.convert_tokens_to_ids("<|eot_id|>")
+    ]
+
+    with torch.no_grad():
+        outputs = model.generate(
+            input_ids,
+            max_length=max_length,
+            eos_token_id=terminators,
+            num_return_sequences=1,
+            temperature=1.0,
+            top_p=0.9,
+            top_k=50,
+            do_sample=True,
+        )
+
+    response = outputs[0][input_ids.shape[-1]:]
+    response = tokenizer.decode(response, skip_special_tokens=True)
+
+    return response
